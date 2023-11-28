0a5e30479122eaab17bb615f2a22d9dbddab986a
Javier de la Rosa <versae@gmail.com>
2023-11-28 12:03:28 +0000

    Add Torgersen Alpaca

--- prepare_dataset_torgersen.py
+++ prepare_dataset_torgersen.py
@@ -1,5 +1,5 @@
 from prepare_dataset import *
-
+from datasets import concatenate_datasets
 
 def generate_prompt(sample):
     if "input" in sample and sample["input"]:
@@ -24,6 +24,19 @@ def generate_prompt(sample):
     sample["prompt"] = promtp
     return sample
 
+
+def generate_prompt_torgersen(sample):
+    promtp = f"""Du er en vennlig chatbot som fungerer som læringsassistent for tannlegestudenter og tannpleiestudenter som skal lære om strålingsfysikk, strålingsbiologi, strålevern og radiologisk teknologi. Ikke gi hele svaret med en gang, men prøv å hjelpe studenten videre et skritt på vei.
+
+### Instruksjon:
+{sample["instruction"]}
+
+### Respons:
+{sample["output"]}"""
+    sample["prompt"] = promtp
+    return sample
+
+
 def main_alpaca(args):
     GPT2TokenizerFast.max_model_input_sizes[
         "gpt2"
@@ -32,16 +45,29 @@ def main_alpaca(args):
     epochs = args.n_repack_epochs
     seq_length = args.sequence_length
 
-    ds = datasets.load_dataset(
+    ds1 = datasets.load_dataset(
         args.dataset,
         name=args.dataset_config or None,
         split=args.dataset_split,
         streaming=False,
         use_auth_token=True,
     )
+    ds1 = ds1.map(generate_prompt, desc="Generating prompts")
+
+    ds2 = datasets.load_dataset(
+        "NbAiLab/torgersen-alpaca",
+        name=None,
+        split="train",
+        streaming=False,
+        use_auth_token=True,
+    )
+    ds2 = ds2.add_column("input", [None] * len(ds2))
+    ds2 = ds2.map(generate_prompt_torgersen, desc="Generating prompts")
+
+    ds = concatenate_datasets([ds1, ds2])
     if not args.preserve_data_order:
         ds = ds.shuffle(seed=args.seed)
-    ds = ds.map(generate_prompt, desc="Generating prompts")
+    #ds = ds.map(generate_prompt, desc="Generating prompts")
     ds = ds.map(lambda x: tokenizer(x["prompt"]), batched=True, desc="Tokenizing", num_proc=16)
     ds.set_epoch = ds.shuffle
     seqs = tqdm(
